426832256eb14c071d58d39a7c9db3c85b2e7ff5
司云霄 <990670204@qq.com>
2020-11-16 10:59:57 +0800

    自动部署

--- .umirc.ts
+++ .umirc.ts
@@ -6,5 +6,7 @@ export default defineConfig({
   favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2746159187,3406008920&fm=15&gp=0.jpg',
   outputPath: 'docs-dist',
+  base: '/goalgo-web-docs',
+  publicPath: '/goalgo-web-docs/',
   // more config: https://d.umijs.org/config
 });
